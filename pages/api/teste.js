303fb768e0605e432db36b0a1b9d42d608e335ec
William isaque <williamisaque18@gmail.com>
2022-09-16 07:23:21 -0300

    ds

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -12,7 +12,7 @@ export default async function pots(req, res) {
       } else {
         let result = await listar();
         res.status(200).json({
-          result,
+          status: "ok",
         });
       }
     } catch (e) {
