f632517731c57e7d5c10fa08e47e7d8cdb2345b5
William isaque <williamisaque18@gmail.com>
2022-09-17 01:20:15 -0300

    ok

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -28,7 +28,7 @@ export default async function pots(req, res) {
       let result = await add(img, title, description, created_at, updated_at);
       console.log("deuuuuuuuu certoooo");
       console.log(result);
-      res.status(200).send();
+      res.status(200).send("ok");
     } catch (e) {
       res.status(500).send(e);
     }
