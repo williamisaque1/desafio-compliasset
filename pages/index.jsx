6c569fbf658ecfabaa067cd12478353b5635d660
William isaque <williamisaque18@gmail.com>
2022-09-16 22:24:33 -0300

    sad

--- pages/index.jsx
+++ pages/index.jsx
@@ -45,7 +45,7 @@ console.log(data)
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Box display={"flex"} overflow={"auto"} flexDirection={"column"} gap={2} >
-          { data ? (data?.map((dataa,i) => {
+          { data ? (data.map((dataa,i) => {
           
             return (
               <Card key={dataa.id} sx={{ maxWidth: 345 }}>
